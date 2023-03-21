b22fd0e54036d425daa2df8627789928f44f921f
ruv <ruv@ruv.net>
2023-03-21 19:25:33 +0000

    Added some functions

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,34 @@
-// Import necessary libraries and modules
 use std::error::Error;
+use reqwest::Client;
+use serde::Deserialize;
 
-// Add OpenAI Codex API library
-// Add GitHub API library
-// Add language-specific testing frameworks
+#[derive(Deserialize, Debug)]
+struct ApiResponse {
+    choices: Vec<Choice>,
+}
 
-// Define the main function
-fn main() -> Result<(), Box<dyn Error>> {
-    // 1. Initialize the OpenAI Codex API
-    //    Set up OpenAI API access and credentials
-    //    Import necessary Rust libraries for API communication
+#[derive(Deserialize, Debug)]
+struct Choice {
+    text: String,
+}
 
-    // 2. Load application function requirements
-    //    Define a list of function requirements as prompts
-    //    Allow users to specify the target programming language
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let function_requirements = vec![
         "function requirement 1",
         "function requirement 2",
         "function requirement 3",
     ];
 
-    // 3. Generate code for each application function
     for function_requirement in &function_requirements {
-        // Call the recursive code generation function
-        let generated_code = generate_code(function_requirement)?;
+        let generated_code = generate_code(function_requirement).await?;
+        println!("\nGenerated code for requirement '{}':\n{}", function_requirement, generated_code);
 
         // 4. Implement a continuous testing feedback loop
-        let error_free_code = test_and_fix_code(generated_code)?;
+        // let error_free_code = test_and_fix_code(generated_code)?;
 
         // 5. Manage and update GitHub branches
-        update_github_branches(error_free_code)?;
+        // update_github_branches(error_free_code)?;
     }
 
     // 6. Monitor progress and exit when complete
@@ -38,19 +37,42 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-// Define the recursive code generation function
-fn generate_code(requirement: &str) -> Result<String, Box<dyn Error>> {
-    // a. Send the prompt to the OpenAI Codex API
-    // b. Receive the generated code from the API
-    // c. Validate the generated code based on the language's syntax
+async fn generate_code(requirement: &str) -> Result<String, Box<dyn Error>> {
+    let api_key = std::env::var("OPENAI_API_KEY")
+        .expect("OPENAI_API_KEY environment variable not found");
 
-    // Placeholder for the actual implementation
+    let client = Client::new();
+    let prompt = format!("Write a Rust function that {}", requirement);
 
-    Ok("Generated code placeholder".to_string())
+    let response = client
+        .post("https://api.openai.com/v1/engines/davinci-codex/completions")
+        .header("Content-Type", "application/json")
+        .header("Authorization", format!("Bearer {}", api_key))
+        .json(&serde_json::json!({
+            "prompt": prompt,
+            "max_tokens": 100,
+            "n": 1,
+            "stop": null,
+            "temperature": 0.5,
+        }))
+        .send()
+        .await?;
+
+    let api_response: ApiResponse = response.json().await?;
+
+    let generated_code = api_response
+        .choices
+        .get(0)
+        .ok_or("No choice found in API response")?
+        .text
+        .trim()
+        .to_string();
+
+    Ok(generated_code)
 }
 
 // Define the test and fix code function
-fn test_and_fix_code(generated_code: String) -> Result<String, Box<dyn Error>> {
+// fn test_and_fix_code(generated_code: String) -> Result<String, Box<dyn Error>> {
     // a. Integrate testing frameworks for the target languages
     // b. Test the generated code for each function
     // c. If errors are found, modify the prompt and re-run code generation
@@ -58,16 +80,16 @@ fn test_and_fix_code(generated_code: String) -> Result<String, Box<dyn Error>> {
 
     // Placeholder for the actual implementation
 
-    Ok("Error-free code placeholder".to_string())
-}
+//     Ok("Error-free code placeholder".to_string())
+// }
 
 // Define the function to manage and update GitHub branches
-fn update_github_branches(code: String) -> Result<(), Box<dyn Error>> {
+// fn update_github_branches(code: String) -> Result<(), Box<dyn Error>> {
     // a. Authenticate with the GitHub API
     // b. Create and switch to new branches for each iteration
     // c. Commit and push code updates to the respective branches
 
     // Placeholder for the actual implementation
 
-    Ok(())
-}
+//     Ok(())
+// }
